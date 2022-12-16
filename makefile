028fc463807ad04d63fc90aca3eba471d7ac4e1e
Cedric Quentin <32091677+cedricq@users.noreply.github.com>
2022-12-16 18:18:17 +0100

    adding unit tests example (#3)
    Co-authored-by: Cedric Quentin <cedric.quentin@wandercraft.eu>

--- makefile
+++ makefile
@@ -13,5 +13,11 @@ project:
 	cd build/$(board_name) && cmake -G "Unix Makefiles" -DARM_TOOLCHAIN_DIR=${ARM_GCC_BIN_FOLDER} -DCMAKE_TOOLCHAIN_FILE=$(ARM_TOOLCHAIN_FILE) -DTOP_BIN_DIR:STRING=$(CURRENT_DIR)/build/$(board_name) -DBOARD_NAME:STRING=$(board_name) -DPRODUCT_NAME:STRING=$(product_name) -DMCU_NAME:STRING=XMC4800_F144x2048 $(CURRENT_DIR)
 	cd build/$(board_name) && $(MAKE) -j8
 
+unit:
+	mkdir -p build/unit
+	cd build/unit && cmake -G "Unix Makefiles" -DTEST:BOOL=True $(CURRENT_DIR)
+	cd build/unit && $(MAKE) -j8
+	cd build/unit && ctest
+	
 clean:
 	rm -rf build
---
+++ test/data_model/DataModel.hpp
@@ -0,0 +1,62 @@
+#ifndef ARC_DATAACCESSOR_HPP
+#define ARC_DATAACCESSOR_HPP
+
+#include <stdint.h>
+
+enum DataItemId
+{
+    // Measurements
+    TIME_ID             = 0,
+
+    // IMU
+    IMU_ACCEL_0_ID,
+    IMU_ACCEL_1_ID,
+    IMU_ACCEL_2_ID,
+    IMU_GYRO_0_ID,
+    IMU_GYRO_1_ID,
+    IMU_GYRO_2_ID,
+    IMU_TEMP_ID,
+
+    // FORCE SENSOR
+    FS_0_ID,
+    FS_1_ID,
+    FS_2_ID,
+    FS_3_ID,
+    FS_4_ID
+
+};
+
+struct Datagram
+{
+    int32_t value;
+    int32_t reset;
+    int32_t min;
+    int32_t max;
+    int32_t div;
+    const char* name;
+};
+
+static Datagram DataItems[] =
+{
+// Measurements
+[TIME_ID]          =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1000,    .name = "time"},
+
+// IMU
+[IMU_ACCEL_0_ID]    =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_accel_0"},
+[IMU_ACCEL_1_ID]    =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_accel_1"},
+[IMU_ACCEL_2_ID]    =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_accel_2"},
+[IMU_GYRO_0_ID]     =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_gyro_0"},
+[IMU_GYRO_1_ID]     =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_gyro_1"},
+[IMU_GYRO_2_ID]     =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_gyro_2"},
+[IMU_TEMP_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "imu_temp"},
+
+// FORCE SENSORS
+[FS_0_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "fs_0"},
+[FS_1_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "fs_1"},
+[FS_2_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "fs_2"},
+[FS_3_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "fs_3"},
+[FS_4_ID]       =    {.value = 0,    .reset = 0,  .min = INT32_MIN,  .max = INT32_MAX,  .div = 1,    .name = "fs_4"},
+
+};
+
+#endif
